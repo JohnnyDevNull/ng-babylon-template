4a64a1cb8d567cb94ec05664ab0221d9553d120c
JohnnyDevNull <philipp.john@live.de>
2019-08-21 04:57:58 +0200

    Upgrade to Angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EngineComponent } from './engine/engine.component';
@@ -22,8 +21,7 @@ import { UiComponent } from './ui/ui.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
   providers: [
   ],
